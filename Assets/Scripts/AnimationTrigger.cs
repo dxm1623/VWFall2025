dd7ff1ab29f37bad515ecec4c384ad5731d41827
dxm1623 <dxm1623@miami.edu>
2025-10-14 18:59:31 -0400

    Build push

--- Assets/Scripts/AnimationTrigger.cs
+++ Assets/Scripts/AnimationTrigger.cs
@@ -1,28 +1,24 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
-// triggers a selected animation when the player touches the trigger
-
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class AnimationTrigger : MonoBehaviour
 {
-    [SerializeField] private PlayableDirector playableDirector; // Reference to the PlayableDirector component
-    [SerializeField] private PlayableAsset timelineAsset; // Reference to the timeline asset to play
-    // public, tag of whatever object(s) that have to touch the trigger to play the animation
-    [SerializeField] private string targetTag = "Player"; // Tag of the player object
+    [SerializeField] private PlayableDirector playableDirector;
+    [SerializeField] private PlayableAsset timelineAsset;
+    [SerializeField] private string targetTag = "Player";
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(targetTag))
         {
-            playableDirector.playableAsset = timelineAsset; // Set the timeline asset
-            playableDirector.Play(); // Play the timeline sequence
+            playableDirector.playableAsset = timelineAsset;
+            playableDirector.Play();
             Debug.Log("Player entered the trigger, playing animation.");
-            // disable the collider to prevent retriggering
             GetComponent<Collider>().enabled = false;
+            
         }
     }
 }
